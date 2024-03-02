c4923258d4265e47ed2afe2781a0fa9f3695be5d
Adrienne Daniels <106035434+Adrienner1988@users.noreply.github.com>
2024-03-01 19:37:30 -0500

    "first commit"

---
+++ Homeshield/src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <h1>Welcome to Homeshield!</h1>
+     <h2>Building tools to make home maintenance a little less of a headache!</h2>
+     <h2>Created by:</h2>
+     <h3>Product Manager: Kat King</h3>
+     <h3>Designer: Polina Vinnikova</h3>
+     <h3>Software Developer: Nathanael Turley</h3>
+     <h3>Software Developer: Adrienne Daniels</h3>
+        
+    </>
+  )
+}
+
+export default App
