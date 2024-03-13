db6500481d2663835d5b650afffe4ad3f34ec343
Adrienne Daniels <106035434+Adrienner1988@users.noreply.github.com>
2024-03-12 20:15:39 -0400

    added component file structure, implemented page route with react router dom, added lables for question inputs

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,28 @@
-import './App.css'
+import "./App.css";
+import "./index.css"
+import Homepage from "./views/Homepage/Homepage";
+import Questions from "./views/Questions/Questions";
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
-  
-
   return (
     <>
-     <h1>Welcome to Homeshield!</h1>
-     <h2>Building tools to make home maintenance a little less of a headache!</h2>
-     <h2>Created by:</h2>
-     <h3>Product Manager: Kat King</h3>
-     <h3>Designer: Polina Vinnikova</h3>
-     <h3>Software Developer: Nathanael Turley</h3>
-     <h3>Software Developer: Adrienne Daniels</h3>
-        
+    <Routes>
+      <Route>
+      <Route path='/' element={<Homepage />}/>
+      <Route path='/' element={<Questions />}/>
+      </Route>
+    </Routes>
+      <h1>Welcome to Homeshield!</h1>
+      <h2>
+        Building tools to make home maintenance a little less of a headache!
+      </h2>
+      <h2>Created by:</h2>
+      <h3>Product Manager: Kat King</h3>
+      <h3>Designer: Polina Vinnikova</h3>
+      <h3>Software Developer: Nathanael Turley</h3>
+      <h3>Software Developer: Adrienne Daniels</h3> 
     </>
-  )
+  );
 }
-
-export default App
+export default App;
---
+++ src/views/Questions/Questions.tsx
@@ -0,0 +1,74 @@
+import "./Questions.css"
+import { useState, ChangeEvent, FormEvent } from "react";
+
+
+const Questions = () => {
+        const [formData, setFormData] = useState({
+            question1: "",
+            question2: "",
+            question3: "",
+            question4: "",
+            question5: "",
+            question6: "",
+            question7: "",
+            question8: "",
+            question9: "",
+            question10: "",
+          });
+          
+          const handleFormSubmit = (event: FormEvent) => {
+            event.preventDefault();
+            console.log(formData);
+          };
+        
+          const handleChange = (event: ChangeEvent) => {
+            const { name, value } = event.target;
+            setFormData((prevFormData) => ({
+              ...prevFormData,
+              [name]: value,
+            }));
+          };
+      return (
+        <>
+            <div className="form-container">
+              <form onSubmit={handleFormSubmit}>
+                <label className="question-labels">Questions 1:</label>
+                <input
+                  type="text"
+                  name="question1"
+                  value={formData.question1}
+                  onChange={handleChange}
+                />
+    
+                <label className="question-labels">Question 2:</label>
+                <input
+                  type="text"
+                  name="question2"
+                  value={formData.question2}
+                  onChange={handleChange}
+                />
+    
+                <label className="question-labels">Question 3:</label>
+    
+                <label className="question-labels">Question 4:</label>
+    
+                <label className="question-labels">Question 5:</label>
+    
+                <label className="question-labels">Question 6:</label>
+    
+                <label className="question-labels">Question 7:</label>
+    
+                <label className="question-labels">Question 8:</label>
+    
+                <label className="question-labels">Question 9:</label>
+    
+                <label className="question-labels">Question 10:</label>
+    
+                <button type="submit">Submit</button>
+              </form>
+            </div>
+          </>
+  )
+}
+
+export default Questions;
