a0bed04c8b5ff04a039235b79590e5609c6faa10
Adrienne Daniels <106035434+Adrienner1988@users.noreply.github.com>
2024-04-06 19:12:34 -0400

    Got the cards to map over each task.

--- src/views/Dashboard/Dashboard.tsx
+++ src/views/Dashboard/Dashboard.tsx
@@ -1,35 +1,45 @@
 import "./Dashboard.css"
-import { HVAC, Caulk, SmokeDetector, BeginnerBadge, HVACBadge, SafetyBadge, ExteriorBadge } from "../../Images";
 import { Link } from "react-router-dom";
 import { FaArrowLeft } from "react-icons/fa6";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 
+interface Task {
+  TaskImageURL: string;
+  TaskName: string;
+  TaskLevel: string;
+  CostDiff: number;
+  MaintenanceType: string;
+  Frequency: string;
+}
 
 const Dashboard = () => {
  const location = useLocation();
- const { formData } = location.state;
+ const [tasks, setTasks] = useState<Task[]>([]);
 
- useEffect(() => {
-     const fetchData = async () => {
-       try {
-         const response = await fetch('https://homeshield-flask.onrender.com/api/list', {
-           method: 'POST',
-           body: JSON.stringify(formData),
-           headers: {
-             'Content-Type': 'application/json'
-           }
-         });
- 
-         const data = await response.json();
-         console.log(data);
-       } catch (error) {
-         console.error('Error fetching data:', error);
-       }
-     };
- 
-     fetchData();
-   }, [formData]);
+useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await fetch('https://homeshield-flask.onrender.com/api/list', {
+          method: 'POST',
+          body: JSON.stringify(location.state.formData),
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        });
+
+        const data = await response.json();
+        setTasks(data);
+        console.log(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    if (location.state && location.state.formData) {
+    getData();
+    }
+  }, [location.state]);
 
   return (
  <>
@@ -50,43 +60,18 @@ const Dashboard = () => {
 </div>
 
 <div className="card-container">
-  <div className="card">
-    <img src={HVAC} alt="Card Image" className="card-img"/>
-    <div className="card-details">
-      <h2>Change your HVAC air filters</h2>
-      <p>Swap out dirty HVAC filters for improved indoor air quality, better airflow, and lower energy bills.</p>
-      <p className="DIY-badge">DIY level: <img src={BeginnerBadge} className="badge-img"/></p>
+    {tasks.map((task, index) => (
+          <div key={index} className="card">
+            <img src={task.TaskImageURL} alt="card image" className="card-img"/>
+            <div className="card-details">
+              <h2>{task.TaskName}</h2>
+              <p className="DIY-badge"> DIY level: {task.TaskLevel}</p>
+              <p className="DIY-badge"> Category: {task.MaintenanceType}</p>
+              <p className="DIY-badge"> Frequency: {task.Frequency}</p>
       
-      
-      <p className="DIY-category">Category: <img src={HVACBadge} className="badge-img"/></p>
-    </div>
-  </div>
-
-  <div className="card">
-    <img src={Caulk} alt="Card Image" className="card-img"/>
-    <div className="card-details">
-      <h2>Caulk windows and doors to seal drafts</h2>
-      <p>Prevent heat loss, lower your energy bills, and keep pests out by caulking gaps around windows and doors.</p>
-
-      <p className="DIY-badge">DIY level: <img src={BeginnerBadge} className="badge-img"/></p>
-      
-      
-      <p className="DIY-category">Category: <img src={SafetyBadge} className="badge-img"/></p>
-    </div>
-  </div>
-
-  <div className="card">
-    <img src={SmokeDetector} alt="Card Image" className="card-img" />
-    <div className="card-details">
-      <h2>Test your smoke and CO detectors</h2>
-      <p>Test the batteries and functionality of your smoke and carbon monoxide detectors for peace of mind.</p>
-
-      <p className="DIY-badge">DIY level: <img src={BeginnerBadge} className="badge-img"/></p>
-      
-      
-      <p className="DIY-category">Category: <img src={ExteriorBadge} className="badge-img"/></p>
-    </div>
-  </div>
+            </div>
+          </div>
+        ))}
 </div>
 
  </>
