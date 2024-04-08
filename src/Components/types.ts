50f0ece21c70db0d55c527435407a9a1fec0fcb2
Nathanael Turley <nbturley16@gmail.com>
2024-04-08 00:27:21 -0400

    Added VideoPlayer.tsx

---
+++ src/Components/types.ts
@@ -0,0 +1,13 @@
+export interface Task {
+    CostDiff: number;
+    DIYVideoLink: string;
+    EstContractorCost: number;
+    EstDIYCost: number;
+    Frequency: string;
+    HouseType: string;
+    MaintenanceType: string;
+    TaskID: string;
+    TaskImageURL: string;
+    TaskName: string;
+    TaskLevel: string;
+  };
