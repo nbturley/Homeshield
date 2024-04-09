7d3ec2cc378c599b868e9c039b7663447e71b00a
Your Name <adriennerdaniels@gmail.com>
2024-04-09 15:34:54 -0400

    Worked on getting the dashboard to rerender the same task when going back to the dashboard from the task page. Worked on passing props from the dashboard to the taak page. Added in a few details on the task page.

--- src/views/TaskPage/TaskPage.tsx
+++ src/views/TaskPage/TaskPage.tsx
@@ -1,26 +1,64 @@
 import "./TaskPage.css";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { FaArrowLeft } from "react-icons/fa6";
 import { VerifiedBadge, TopRatedBadge } from "../../Images";
 import YouTubePlayer from "./VideoPlayer";
+import { Task } from "../../Components/types";
+// import { BeginnerBadge, intermediateBadge, AdvanceBadge } from "../../Images";
 
-const TaskPage: React.FC = () => {
+
+interface TaskPageProps {
+  task: Task;
+}
+
+const TaskPage: React.FC<TaskPageProps> = ({ task }) => {
+const navigate = useNavigate();
+
+const handleBackToDashboard = () => {
+  navigate("/Dashboard");
+};
 
   return (
     <>
       <div className="arrow-div">
-        <Link to={"/Dashboard"}>
-          <button className="task-back-arrow">
+          <button className="task-back-arrow" onClick={handleBackToDashboard}>
             <FaArrowLeft />
           </button>
-        </Link>
       </div>
 
       <div className="left-div">
         <div className="video-box">
-            <h1>Task Name</h1>
+            <h1 className="learn-how">Learn How: {task && task.TaskName}</h1>
+            {/* <p className="DIY-badge"> DIY level:   
+              {task.TaskLevel === "Beginner" && <img src={BeginnerBadge} alt={task.TaskLevel} />}
+              
+              {task.TaskLevel === "Intermediate" && <img src={intermediateBadge} alt={task.TaskLevel} />}
+
+              {task.TaskLevel === "Advanced" && <img src={AdvanceBadge} alt={task.TaskLevel} />}</p> */}
+
             <YouTubePlayer videoId="ED3bfesbE_Y?si=ZzZMnRKjT4g-8iYo" />
         </div>
+
+        <div>
+          <h1 className="what-you-need">Everything you'll need</h1>
+          <form action="checklist">
+            <input type="checkbox" id="tools-needed" name="tools" value="needs"/>
+            <label htmlFor="tools">New filter that fits your furnace</label>
+
+            <input type="checkbox" id="tools-needed" name="tools" value="needs"/>
+            <label htmlFor="tools">Sharpie marker</label>
+
+            <input type="checkbox" id="tools-needed" name="tools" value="needs"/>
+            <label htmlFor="tools">Screwdriver or pliers
+</label>
+          </form>
+        </div>
+
+        <div>
+          <h1 className="DIY-or-pro">Should you DIY or Hire a pro?</h1>
+          <p className="decision">We know trying a new service out can be scary. We break down the factors to consider for the best decision for your home.</p>
+          </div>
+
       </div>
 
       <div className="right-div">
